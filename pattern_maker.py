6b02a4b9e4318f22568cc2a40aa2fe50da4f6749
Raja CSP Raman <raja.raman@syndigo.com>
2023-07-17 13:07:23 +0530

    Refactored the code for better readability (now it's just incomprehensible): Updates for 2023-07-17

--- pattern_maker.py
+++ pattern_maker.py
@@ -9,6 +9,8 @@ source:
     
     Address-CNER
     https://docs.google.com/spreadsheets/d/1QH_T6C3MAJNj5f_1gL1dZtLRUjFbwJ4wcHpPwlCfylI/edit#gid=968221325
+
+    https://www.kaggle.com/rajacsp/address-pattern/
 '''
 
 def get_spaces(content):
@@ -24,6 +26,9 @@ def get_spaces(content):
     
     if(len(content) < 16):
         return (" " * (16 - len(content)))
+    
+    if(len(content) < 20):
+        return (" " * (20 - len(content)))
 
     return ""
 
@@ -85,63 +90,10 @@ def pattern_2_maker_single(address):
         content += "STREET_NAME"
         content += "\n"
 
-    # content += f"{address_parts[1]}"
-    # content += f"{get_spaces(address_parts[1])}"
-    # content += "STREET_NAME"
-    # content += "\n"
-
-    # content += f"{address_parts[2]}"
-    # content += f"{get_spaces(address_parts[2])}"
-    # content += "STREET_NAME"
-
-    # print(content)
-
     return content
 
 
-def pattern_13_maker_single(address):
 
-    '''
-        Pattern 13:
-
-        1	
-        (House_No) (StreetName) (UNIT) (Suite_No)
-        
-        Sample:
-        23 CHEVIN Road UNIT 90
-    '''
-
-    address_parts = address.split(" ")
-
-    content = ""
-
-    content += f"{address_parts[0]}"
-    content += f"{get_spaces(address_parts[0])}"
-    content += "HOUSE_NO"
-    content += "\n"
-
-    content += f"{address_parts[1]}"
-    content += f"{get_spaces(address_parts[1])}"
-    content += "STREET_NAME"
-    content += "\n"
-
-    content += f"{address_parts[2]}"
-    content += f"{get_spaces(address_parts[2])}"
-    content += "STREET_NAME"
-    content += "\n"
-
-    content += f"{address_parts[3]}"
-    content += f"{get_spaces(address_parts[3])}"
-    content += "SUITE_NO"
-    content += "\n"
-
-    content += f"{address_parts[4]}"
-    content += f"{get_spaces(address_parts[4])}"
-    content += "SUITE_NO"
-
-    # print(content)
-
-    return content
 
 def pattern_3_maker_single(address):
 
@@ -184,6 +136,101 @@ def pattern_3_maker_single(address):
 
     return content
 
+def pattern_4_maker_single(address):
+
+    '''
+        Pattern 4:
+
+        4	
+        (Suite_No)-(House_No) (StreetName_with_Number)	
+        1701-1799 HIGHWAY 2 W
+
+        Sample:
+        1701-1799 HIGHWAY 2 W
+    '''
+
+    address_parts = address.split(" ")
+
+    content = ""
+
+    sub_parts = address_parts[0].split("-")
+
+    content += f"{sub_parts[0]}"
+    content += f"{get_spaces(sub_parts[0])}"
+    content += "SUITE_NO"
+    content += "\n"
+
+    content += f"-"
+    content += f"{get_spaces('-')}"
+    content += "0"
+    content += "\n"
+
+    content += f"{sub_parts[1]}"
+    content += f"{get_spaces(sub_parts[1])}"
+    content += "HOUSE_NO"
+    content += "\n"
+
+    content += f"{address_parts[1]}"
+    content += f"{get_spaces(address_parts[1])}"
+    content += "STREET_NAME"
+    content += "\n"
+
+    content += f"{address_parts[2]}"
+    content += f"{get_spaces(address_parts[2])}"
+    content += "STREET_NAME"
+
+    return content
+
+def pattern_5_maker_single(address):
+
+    '''
+        Pattern 5:
+
+        5	
+        (Suite_No)/(House_No) (StreetName)	
+        55/57 BAHNHOFSTRASSE
+
+        Sample:
+        1/3 WESTGATE PARK FODDERWICK
+    '''
+
+    # print(address)
+
+    address_parts = address.split(" ")
+
+    # print(len(address_parts))
+
+    content = ""
+
+    for c_index, c_item in enumerate(address_parts):
+        # print(c_index, c_item)
+
+        if(c_index == 0):
+
+            sub_parts = c_item.split("/")
+
+            content += f"{sub_parts[0]}"
+            content += f"{get_spaces(sub_parts[0])}"
+            content += "SUITE_NO"
+            content += "\n"
+
+            content += f"-"
+            content += f"{get_spaces('/')}"
+            content += "0"
+            content += "\n"
+
+            content += f"{sub_parts[1]}"
+            content += f"{get_spaces(sub_parts[1])}"
+            content += "HOUSE_NO"
+            content += "\n"
+        else:
+            content += f"{c_item}"
+            content += f"{get_spaces(c_item)}"
+            content += "STREET_NAME"
+            content += "\n"
+
+    return content
+
 def pattern_10_maker_single(address):
 
     '''
@@ -213,6 +260,50 @@ def pattern_10_maker_single(address):
     content += f"{address_parts[2]}"
     content += f"{get_spaces(address_parts[2])}"
     content += "STREET_NAME"
+
+    # print(content)
+
+    return content
+
+def pattern_13_maker_single(address):
+
+    '''
+        Pattern 13:
+
+        1	
+        (House_No) (StreetName) (UNIT) (Suite_No)
+        
+        Sample:
+        23 CHEVIN Road UNIT 90
+    '''
+
+    address_parts = address.split(" ")
+
+    content = ""
+
+    content += f"{address_parts[0]}"
+    content += f"{get_spaces(address_parts[0])}"
+    content += "HOUSE_NO"
+    content += "\n"
+
+    content += f"{address_parts[1]}"
+    content += f"{get_spaces(address_parts[1])}"
+    content += "STREET_NAME"
+    content += "\n"
+
+    content += f"{address_parts[2]}"
+    content += f"{get_spaces(address_parts[2])}"
+    content += "STREET_NAME"
+    content += "\n"
+
+    content += f"{address_parts[3]}"
+    content += f"{get_spaces(address_parts[3])}"
+    content += "SUITE_NO"
+    content += "\n"
+
+    content += f"{address_parts[4]}"
+    content += f"{get_spaces(address_parts[4])}"
+    content += "SUITE_NO"
 
     # print(content)
 
@@ -259,6 +350,10 @@ def pattern_maker_single(c_line, pattern_index):
         return pattern_2_maker_single(c_line)
     elif(pattern_index == 3):
         return pattern_3_maker_single(c_line)
+    elif(pattern_index == 4):
+        return pattern_4_maker_single(c_line)
+    elif(pattern_index == 5):
+        return pattern_5_maker_single(c_line)
     elif(pattern_index == 10):
         return pattern_10_maker_single(c_line)
     elif(pattern_index == 13):
@@ -299,13 +394,38 @@ def startpy():
     # print(pattern_maker_multiple(1))
 
     # Pattern 2
-    print(pattern_maker_multiple(2))
+    # print(pattern_maker_multiple(2))
 
     # Pattern 3
     # print(pattern_maker_multiple(3))
 
+    # Pattern 4
+    # print(pattern_maker_multiple(4))
+
+    # Pattern 5
+    # pattern_5_maker_single("1/3 WESTGATE PARK FODDERWICK")
+    print(pattern_maker_multiple(5))
+
+    # Pattern 6
+    # print(pattern_maker_multiple(6))
+
+    # Pattern 7
+    # print(pattern_maker_multiple(7))
+
+    # Pattern 8
+    # print(pattern_maker_multiple(8))
+
+    # Pattern 9
+    # print(pattern_maker_multiple(9))
+
     # Pattern 10
     # print(pattern_maker_multiple(10))
+
+    # Pattern 11
+    # print(pattern_maker_multiple(11))
+
+    # Pattern 12
+    # print(pattern_maker_multiple(12))
 
     # Pattern 13
     # print(pattern_maker_multiple(13))
