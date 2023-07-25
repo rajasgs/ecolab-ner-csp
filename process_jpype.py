b99c1f79db09fa49f60a16e38f1f3007a21ce2b0
Raja CSP Raman <raja.raman@syndigo.com>
2023-07-25 14:44:06 +0530

    Adding the kitchen sink because why not: Updates for 2023-07-25

--- process_jpype.py
+++ process_jpype.py
@@ -30,9 +30,9 @@ def get_tokens(address):
     # address = address.replace("'", "")
     # address = address.replace("&", "and")
 
-    INPUT = address.replace("'", "")
+    # INPUT = address.replace("'", "")
 
-    result = SampleClass.getTokens(address)
+    result = SampleClass.getTokens(str(address))
 
     # print(result)
 
@@ -62,7 +62,6 @@ def startpy():
 
     start_time = datetime.now()
 
-    jvm_dir = "/home/rajaraman/rprojects/ecolab-ner-csp/"
     jpype.startJVM(classpath = ['jars/*', "/home/rajaraman/rprojects/ecolab-ner-csp/"])
     
     # result = get_tokens("152 ST ANNE'S RD")
@@ -72,17 +71,31 @@ def startpy():
 
     df = pd.read_csv('ver-2023-01_test.csv')
 
-    street_name_list = []
-    house_no_list = []
-    suite_no_list = []
+    street_name_list    = []
+    house_no_list       = []
+    suite_no_list       = []
+
+    street_name_right_list    = []
+    house_no_right_list       = []
+    suite_no_right_list       = []
     for index, row in df.iterrows():
         # print(f'{index} : {row["ADDRESS"]}')
 
-        token_dict = get_tokens(row["ADDRESS"])
+        address_token_dict = get_tokens(row["ADDRESS"])
 
-        street_name_list.append(token_dict['STREET_NAME'])
-        house_no_list.append(token_dict['HOUSE_NO'])
-        suite_no_list.append(token_dict['SUITE_NO'])
+        street_name_list.append(address_token_dict['STREET_NAME'])
+        house_no_list.append(address_token_dict['HOUSE_NO'])
+        suite_no_list.append(address_token_dict['SUITE_NO'])
+
+        address_right_token_dict = get_tokens(row["ADDRESS_RIGHT"])
+
+        street_name_right_list.append(address_right_token_dict['STREET_NAME'])
+        house_no_right_list.append(address_right_token_dict['HOUSE_NO'])
+        suite_no_right_list.append(address_right_token_dict['SUITE_NO'])
+
+    df.insert(8,'STREET_NAME_RIGHT', street_name_right_list)
+    df.insert(9,'HOUSE_NO_RIGHT', house_no_right_list)
+    df.insert(10,'SUITE_NO_RIGHT', suite_no_right_list)
 
     df.insert(3,'STREET_NAME', street_name_list)
     df.insert(4,'HOUSE_NO', house_no_list)
