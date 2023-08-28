67cb895ee1ec7c6bad8264dc45d3335c785d5eb6
Raja CSP Raman <raja.raman@syndigo.com>
2023-08-28 06:51:29 +0530

    Refactored the code for better readability (now it's just incomprehensible): Updates for 2023-08-28

--- ner.py
+++ ner.py
@@ -143,8 +143,8 @@ def startpy():
     # train_model("ecolab_address_20230720", ["Ecolab_Address_Training_Ver1.txt"], ["Ecolab_Address_Testing_Ver1.txt"], True, classpath)
 
     train_model(
-        "ecolab_address_20230828_2", 
-        ["Ecolab_Address_Training_Pattern1_2023_08_28_2.txt"], 
+        "ecolab_address_20230828_3", 
+        ["Ecolab_Address_Training_Pattern1_2023_08_28_3.txt"], 
         ["Ecolab_Address_Testing_Pattern1_2023_08_28_2.txt"], 
         True, 
         classpath
