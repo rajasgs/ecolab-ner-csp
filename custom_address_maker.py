38c0ca2e7a5cd2bfcebd2e873c88710a5df86e19
Raja CSP Raman <raja.raman@syndigo.com>
2023-10-23 09:11:44 +0530

    Added a 'fix' for the previous 'fix' that broke everything: Updates for 2023-10-23

--- custom_address_maker.py
+++ custom_address_maker.py
@@ -12,6 +12,12 @@ source:
     
 '''
 import random
+import sys
+from importlib import import_module
+
+MAX_ADDRESS_COUNT = 50
+
+dynamic_module  = import_module(f"custom_address_maker")
 
 address_part_2023101801_str = """
 dover, beacon, sylvester, winn, winnmere, sunnyside
@@ -22,11 +28,12 @@ address_part_10002_str = """
 """
 
 address_part_13_20231023_1 = """
-chanute, travis, main, elm, maple, oak, birch, cedar, spruce, pine, redwood, spruce, willow
+chanute, travis, main, elm, maple, oak, birch, cedar, spruce, pine, redwood, spruce, willow, marshfield, terrace, wyresdale, 
+neville, laity, temple, shaftesbury, hills, patrick, bentrim
 """
 
 address_part_13_20231023_2 = """
-rd, road, st, street, ave, avenue, lane, crt, court, drive, dr
+rd, road, st, street, ave, avenue, lane, crt, court, drive, dr, causeway,
 """
 
 address_part_13_20231023_3 = """
@@ -35,9 +42,21 @@ unit, unt, unit, unit
 
 def get_random_part(content):
 
-    contents = content.lower().strip().split(',')
+    contents = content.lower().strip()
 
-    return str(random.choice(contents)).strip()
+    content = ','.join(word for word in content.split(',') if len(word)>0)
+    content_list = content.split(',')
+
+    new_content_list = []
+
+    for item in content_list:
+        item = item.strip().replace('\n', '')
+        
+        if(len(item) < 1):
+            continue
+        new_content_list.append(item)
+
+    return str(random.choice(new_content_list)).strip()
 
 def get_house_no():
 
@@ -68,22 +87,36 @@ def pattern_13_20231023():
     part4 = grp(address_part_13_20231023_3)
     part5 = get_house_no()
 
-    address_part_13_20231023_3
-
     return f"{part1} {part2} {part3} {part4} {part5}"
 
-def get_multiple_addresses():
+def pattern_2_maker_single():
+
+    '''
+        marshfield bank
+    '''
+
+    part1 = grp(address_part_13_20231023_1)
+    part2 = grp(address_part_13_20231023_2)
+
+    return f"{part1} {part2}"
+
+def get_multiple_addresses(pattern_index):
+
     
-    for _ in range(50):
-        # print(pattern_2023101801())
-
-        print(pattern_13_20231023())
-
-        pass
+    dynamic_method  = getattr(dynamic_module, f"pattern_{pattern_index}_maker_single")
+    
+    for _ in range(MAX_ADDRESS_COUNT):
+        print(dynamic_method())
 
 def startpy():
+
+    pattern = int(sys.argv[1])
     
-    get_multiple_addresses()
+    # get_multiple_addresses(pattern)
+
+    for _ in range(100):
+
+        print(grp(address_part_13_20231023_2))
 
 if __name__ == '__main__':
     startpy()
