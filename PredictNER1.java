1447743cc8fab979f09c8bde15c7ac7819ee6c4c
Raja CSP Raman <raja.raman@syndigo.com>
2023-12-27 17:24:44 +0530

    Changed all numbers to be written in binary because why not: Updates for 2023-12-27

--- PredictNER1.java
+++ PredictNER1.java
@@ -25,7 +25,7 @@ import java.util.List;
 
 public class PredictNER1 {
 
-    private static final String MODEL_PATH = "ecolab_address_20231026_1.model.ser.gz";
+    private static final String MODEL_PATH = "ecolab_address_bca_20231227_1.model.ser.gz";
 
     static void print(Object obj){
         System.out.println(obj);
